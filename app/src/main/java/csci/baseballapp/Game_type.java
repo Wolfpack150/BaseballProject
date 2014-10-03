67f52b00ea0b6d067bdb26e4637a8d631bfc891d
tim <herrschwartz@gmail.com>
2014-10-02 19:04:42 -0700

    Added Activities, Layouts, pancakes...

--- app/src/main/java/csci/baseballapp/Game_type.java
+++ app/src/main/java/csci/baseballapp/Game_type.java
@@ -1,7 +1,10 @@
 package csci.baseballapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by tim on 9/18/14.
@@ -12,5 +15,21 @@ public class Game_type extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_type);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+        {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void gotoLineup(View v) {
+        Intent intent = new Intent(this, team_lineups.class);
+        startActivity(intent);
+
     }
 }
