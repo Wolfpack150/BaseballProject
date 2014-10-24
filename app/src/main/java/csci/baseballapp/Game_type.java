a8b6f26d879e2094f3a7377471b51cde95ef6e2e
bvlundberg@gmail.com <bvlundberg@gmail.com>
2014-10-23 18:49:31 -0700

    Set up the variables and intent to send information to the create lineups page

--- app/src/main/java/csci/baseballapp/Game_type.java
+++ app/src/main/java/csci/baseballapp/Game_type.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by tim on 9/18/14.
@@ -14,25 +15,25 @@ import android.widget.EditText;
 
 public class Game_type extends Activity {
 
-    EditText homeTeamName, visitorTeamName, numberInnings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_type);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        homeTeamName = (EditText) findViewById(R.id.homeNameInput);
-        visitorTeamName = (EditText) findViewById(R.id.visNameInput);
-        numberInnings = (EditText) findViewById(R.id.visNameInput);
-        Button button = (Button) findViewById(R.id.button);
 
+
+
+    }
+        /*
+        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 Intent intent = new Intent(Game_type.this, team_lineups.class);
                 startActivity(intent);
             }
-        });
-    }
+        });*/
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -43,26 +44,23 @@ public class Game_type extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void setGametype (View v)
-    {
-        Gameplay game = new Gameplay();
-        Team homeTeam = new Team();
-        Team visitorTeam = new Team();
+    public void gotoLineup(View view) {
+        EditText homeTeamName, visitorTeamName, numberInnings;
 
-        //Intent intent = new Intent();
-        //This gets the data from an EditText field
-        String homeName = homeTeamName.getText().toString();
-        homeTeam.setTeamName(homeName);
-        //intent.putExtra("Home", homeName);
-        String visitorName = visitorTeamName.getText().toString();
-        //intent.putExtra("Visitor", visitorName);
-        visitorTeam.setTeamName(visitorName);
-        int innings = Integer.parseInt(numberInnings.getText().toString());
-        //intent.putExtra("Innings",innings);
-        game.m_numInnings = innings;
+        homeTeamName = (EditText) findViewById(R.id.homeNameInput);
+        visitorTeamName = (EditText) findViewById(R.id.visNameInput);
+        numberInnings = (EditText) findViewById(R.id.visNameInput);
 
-        //setResult(RESULT_OK, intent);
-        finish();
+        String homeName, visName, gameInnings;
+        homeName = String.valueOf(homeTeamName.getText());
+        visName = String.valueOf(visitorTeamName.getText());
+        gameInnings = String.valueOf(numberInnings.getText());
+
+        Intent goToCreateLineup = new Intent(this, team_lineups.class);
+        goToCreateLineup.putExtra("HomeTeamName", homeName);
+        goToCreateLineup.putExtra("VisitorTeamName", visName);
+        goToCreateLineup.putExtra("NumberInnings", gameInnings);
+
+        setResult(RESULT_OK, goToCreateLineup);
     }
-
 }
