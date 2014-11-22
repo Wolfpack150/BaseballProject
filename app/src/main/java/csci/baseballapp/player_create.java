923d2e42148b457bf4a51a9851928710cfa45f99
ArinL <shmi117@mail.fresnostate.edu>
2014-11-22 15:35:31 -0800

    Merge branch 'Development' of https://github.com/Wolfpack150/BaseballProject into Interface
    Conflicts:
    	app/src/main/res/layout/activity_box.xml
    	app/src/main/res/layout/activity_play_ball.xml

--- app/src/main/java/csci/baseballapp/player_create.java
+++ app/src/main/java/csci/baseballapp/player_create.java
@@ -11,6 +11,8 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.view.MotionEvent;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -151,6 +153,28 @@ public class player_create extends Activity {
 
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        View view = getCurrentFocus();
+        boolean ret = super.dispatchTouchEvent(event);
+
+        if (view instanceof EditText) {
+            View w = getCurrentFocus();
+            int scrcoords[] = new int[2];
+            w.getLocationOnScreen(scrcoords);
+            float x = event.getRawX() + w.getLeft() - scrcoords[0];
+            float y = event.getRawY() + w.getTop() - scrcoords[1];
+
+            if (event.getAction() == MotionEvent.ACTION_UP
+                    && (x < w.getLeft() || x >= w.getRight()
+                    || y < w.getTop() || y > w.getBottom()) ) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getWindow().getCurrentFocus().getWindowToken(), 0);
+            }
+        }
+        return ret;
     }
 
 }
