e20fcae17df5ac1b88b651160d49a25302693bdf
bvlundberg@gmail.com <bvlundberg@gmail.com>
2014-11-22 20:28:55 -0800

    Pitch menu, balls/strikes, switch inning

--- app/src/main/java/csci/baseballapp/PlayBall.java
+++ app/src/main/java/csci/baseballapp/PlayBall.java
@@ -2,12 +2,21 @@ package csci.baseballapp;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 
 public class PlayBall extends Activity {
@@ -73,8 +82,67 @@ public class PlayBall extends Activity {
         return super.onOptionsItemSelected(item);
     }
     public void showDialog(View view){
+        class PitchDialogFragment extends DialogFragment {
+            public PitchDialogFragment() {}
+            @Override
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+                alertDialogBuilder.setTitle("Pick");
+                alertDialogBuilder.setItems(R.array.pitchList, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getActivity(), "Item was selected " + i, Toast.LENGTH_SHORT).show();
+                        pitchListener1(i);
+                    }
+                });
+
+                return alertDialogBuilder.create();
+            }
+
+        }
         PitchDialogFragment pitchDialog = new PitchDialogFragment();
         pitchDialog.show(getFragmentManager(), "Pitch Dialog");
     }
-
+    private void pitchListener1(int result) {
+        switch (result) {
+            case 0:
+                game.incrementBall();
+                updateGameView();
+                break;
+            case 1:
+                game.incrementStrike();
+                updateGameView();
+                break;
+            case 2:
+                game.foulball();
+                updateGameView();
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }
+    private void updateGameView(){
+        TextView visName = (TextView) findViewById(R.id.visName);
+        visName.setText(game.m_away.m_teamName);
+        TextView homeName = (TextView) findViewById(R.id.homeName);
+        homeName.setText(game.m_home.m_teamName);
+        TextView inningType = (TextView) findViewById(R.id.innTypeView);
+        inningType.setText(game.inningToString());
+        TextView currentInning = (TextView) findViewById(R.id.innCurrView);
+        currentInning.setText(String.valueOf((int) game.m_inning));
+        TextView homeScore = (TextView) findViewById(R.id.homeScore);
+        homeScore.setText(String.valueOf(game.m_home_score));
+        TextView visScore = (TextView) findViewById(R.id.visScore);
+        visScore.setText(String.valueOf(game.m_away_score));
+        TextView ballCount = (TextView) findViewById(R.id.ballCountView);
+        ballCount.setText(String.valueOf(game.m_balls));
+        TextView strikeCount = (TextView) findViewById(R.id.strikeCountView);
+        strikeCount.setText(String.valueOf(game.m_strikes));
+        TextView outCount = (TextView) findViewById(R.id.outCountView);
+        outCount.setText(String.valueOf(game.m_outs));
+    }
 }
+
